c1ed8d14e7d5072b22d626595c66577f3b8f1f97
guntasreelekha <guntasreelekha@gmail.com>
2020-09-12 07:39:00 +0530

    Logical operators

---
+++ LogicalNot.java
@@ -0,0 +1,13 @@
+package operators;
+
+public class LogicalNot {
+    public static void main(String args[]) {
+        int a = 10, b = 1;
+        // Displaying a, b, c
+        System.out.println("Var1 = " + a);
+        System.out.println("Var2 = " + b);
+        // Using logical NOT operator
+        System.out.println("!(a < b) = " + !(a < b));
+        System.out.println("!(a > b) = " + !(a > b));
+    }
+}
---
+++ ShiftOperator.java
@@ -0,0 +1,10 @@
+package operators;
+
+public class ShiftOperator {
+    public static void main(String args[])  {
+        int x = -4;
+        System.out.println(x>>1);
+        int y = 4;
+        System.out.println(y>>1);
+    }
+}
